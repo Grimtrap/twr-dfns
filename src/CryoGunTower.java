bd3bb6f3292317a1a09a2c2db69893ff5ace29a5
335413373 <335413373@users.noreply.github.com>
2019-01-08 11:19:19 -0500

    need specific values

--- src/CryoGunTower.java
+++ src/CryoGunTower.java
@@ -1,3 +1,7 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class CryoGunTower extends Tower {
 
     public CryoGunTower(double x, double y) {
@@ -10,6 +14,16 @@ public class CryoGunTower extends Tower {
     }
 
     public void attack(){
-
+        //need to create an array of enemies within its range
+        //fires at the enemy closest to base
+        //Creates action listener updates projectile based on timer
+        ActionListener shoot = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //new Projectile();
+            }
+        };
+        Timer t = new Timer ((int)(rate), shoot);
+        t.start();
     }
 }
