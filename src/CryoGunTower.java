b09104ab3a6d96cde7f6eebe404d9e0ff4f57c7c
335413373 <335413373@users.noreply.github.com>
2019-01-15 10:28:16 -0500

    range needed

--- src/CryoGunTower.java
+++ src/CryoGunTower.java
@@ -21,6 +21,8 @@ public class CryoGunTower extends Tower {
         //create an array of enemies within its range
         setEnemies(findTargets());
         //fires at the enemy closest to base
+        this.setTargetX(this.getEnemies().get(0).getX());
+        this.setTargetY(this.getEnemies().get(0).getY());
         //Creates action listener updates projectile based on timer
         ActionListener shoot = new ActionListener() {
             @Override
